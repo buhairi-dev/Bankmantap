9664f6226cca5fc147e6b608179320421b343694
Adminlocal <buhairi_mail@yahoo.com>
2021-03-08 14:13:58 +0700

    update

--- sekilasperusahaan.php
+++ sekilasperusahaan.php
@@ -1,18 +1,98 @@
 <?php include 'layout/header.php';?>
 
-<div class="container" style="top: 90px;
-left: 0px;
-width: 1450px;
-height: 410px;">
+ 
 <img src="asset/slider2.png">
-</div>
+ 
 
-<div class="container">
-<section>
-    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis eius, facilis optio libero porro, soluta aspernatur quod quibusdam quae non, fugit mollitia natus incidunt ea delectus nostrum veniam at officia?</h1>
+<section class="section">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <h4 style="color:#0F2B5B;">Visi Perusahaan</h4>
+        <small>Menjadi mitra terbaik dan terpercaya dalam memberdayakan dan mensejahterakan</small>
+        <div class="space"></div>
+
+        <h4>Misi Perusahaan</h4>
+        <ul>
+          <li>Menciptakan pertumbuhan bisnis dan kesejahteraan melalui produk dan layanan keuangan terbaik</li>
+          <li>Fokus pada kebutuhan UMKM dan Pensiunan dengan menyelaraskan kepentingan ekonomi, sosil dan lingkungan</li>
+        </ul>
+      </div>
+      <div class="col-sm-4">
+        <img src="asset/img/2_1.JPG"  alt="Logo" width="400px">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <h4 style="color:#0F2B5B;">Riwayat Singkat Perusahaan</h4>
+        <p><small>
+        PT Bank Mandiri Taspen (selanjutnya disebut "Bank) didirikan di Denpasar pada tanggal 3 November 1992 berdasarkan Akta Pendirian No.4, yang dibuat di hadapan Ida Bagua Alit Sudiatmika, S.H, Notrasi
+        di Denpasar dengan nama PT Bank Sinar Harapan Bali. Pendirian ini merupakan peningkatan status badan hukum yang sebelumnya berbadan hukum yang sebelumnya berbadan hukum Maskapai Andil Indonesia (MAI) menjadi Perseroan Terbatas.
+        Akta pendirian ini telah mendapat pengesahan dari Menteri Kehakiman Republik Indonesia dengan Surat Keputusan No. C2-4581HT.01.01 Th. 93 tertanggal 12 Juni 1993.
+        </small></p>
+
+        <p><small>
+        Dalam rangka bertransformasi untuk menjadi Bank Nasional, berdasarkan keputusan RUPS Luar Biasa tanggal 22 Desember 2014 sebagaimana yang dituangkan dalam Akta No. 93 tanggal 22 Desember 2014
+        yang dibuat di hadapan Notaris I Gusti Ngurah Putra Wijaya, S.H, dilakukan perubahan modal dasar, modal ditempatkan dan modal disetor Bank dari Rp100.000.000.000 menjadi Rp500.000.000.000 serta 
+        perubahan susunan pemegang saham, Akta perubahan tersebut telah mendapat persetujuan sesuai dengan Surat Keputusan Menteri Hukum dan Hak Asasi Manusia No. AHU-13665.40.20.214 tertanggal 30 Desember 2014.
+        </small></p>
+
+        <p><small>
+        Dengan masuknya Pemegang Saham baru yaitu PT Taspen (Persero) dan PT Pos Indonesia (Persero), diputuskan pula perubahan nama Bank dari PT Bank Sinar Harapan Bali menjadi PT Bank Mandiri 
+        Taspen Pos berdasarkan perubahan Anggaran Dasar sesuai Akta No. 5 tanggal 7 Januari 2015 yang dibuat di hadapan Notrasi I Gustu Ngurah Putra Wijaya, S.H. Notaris di Denpasar. Akta perubahan tersebut
+        telah di setujui oleh Menteri Hukum dan Hak Asasi Manusia Republik Indonesia melalui Surat keputusan No. AHU-0001075.AH.01.02 tanggal 23 Januari 2015. Perubahan nama Bank tersebut juga telah
+        disetujui oleh Otoritas Jasa Keuangan (OJK) berdasarkan surat no. 166/KDK.03/2015 tanggal 24 Juli 2015 tentang Penggunaan ijin Usaha atas nama PT Bank Mandiri Taspen Pos dan surat No. S-134/
+        KO.311/2015 tanggal 31 Juli 2015 tentang Persetujuan Perubahan Logo, maka PT Bank Sinar Harapan Bali secara resmi berganti nama dan logo menjadi PT Bank Mandiri Taspen Pos pada tanggal 7 Agustus 2015.
+        </small></p>
+
+        <p><small>
+        Sesuai dengan Keputusan Rapat Umum Pemegang Saham No.4 tanggal 6 November 2017 dari Notaris I Gustu Ngurah Putra Wijaya S.H, notaris di Denpasa, pemegang saham Bank menyetujui perubahan
+        nama bank dari PT Bank Mandiri Taspen Pos menjadi PT Bank Mandiri Taspen. Akta perubahan tersebut telah dilaporkan kepada Menteri Hukum dan Hak Asasi Manusia Republik Indonesia dengan surat penerimaan pemberitahuan perubahan data Bank tertanggal 7 November 2017 No. AHU-AH.01.03-0188167 dan telah di daftarkan pada daftar Perseroan Nomor AHU-0140174.AH.01.11 tahun 2017 tanggal 7
+        november 2017.
+        </small></p>
+
+        <p><small>
+        Perubahan nama Bank tersebut juga telah disetujui oleh OJK berdasarkan surat No. KEP-22/PB.1/2017 tanggal 18 Desember 2017 tentang Penetapan Penggunaan Ijin Usaha atas nama PT Bank Mandiri Taspen
+        Pos menjadi PT Bank Mandiri Taspen dan surat OJK Regional : Balik dan Nusa Tenggara No. S-128/KR.08/2017 tanggal 22 Desember 2017 tentang Penegasan perubahan logo Bank, maka PT Bank Mandiri
+        Taspen Pos secara resmi berganti nama dan logo menjadi PT Bank Mandiri Taspen pada tanggal 23 Desember 2017.
+        </small></p>
+
+        <p><small>
+        Untuk menguatkan ekspansi usaha dan menjadi Bank Nasional yang memiliki jaringan usaha di seluruh pelosok Indonesia, berdasarkan Keputusan Rapat Umum Pemegang Saham No.53 tanggal 31 oktober 2016 dari
+        Notaris I Gusti Ngurah Putra Wijaya, S.H., Notaris di Denpasar, akta mana kemudian ditegaskan dengan Penegasan Keputusan Rapat nomor 7 tanggal 5 Maret 2019 mengenai Pemindahan Kedudukan Kantor Pusat
+        Bank. Akta perubahan tersebut telah dilaporkan kepada Menteri Hukum dan Hak Asasi Manusia Republik Indonesia sebagaimana surat penerimaan pembertahuan perubahan data Perseroan tertanggal 11 
+        Maret 2019 No. AHU-AH.01.03-138220 dan telah di daftarkan pada daftar Perseroan Nomor AHU-0039461.AH.01.11 Tahun 2019 tanggal 11 Maret 2019. Perubahan Anggaran Dasar tersebut juga telah disetujui
+        berdasarkan Keputusan Menteri Hukum dan Hak Asasi Manusia Republik Indonesia No.AHU-0012925.AH.01.02 tahun 2019 tentang Persetujuan Perubahan Anggaran Dasar Perseroan Terbatas PT Bank Mandiri Taspen 
+        serta telah mendapat persetujuan dari Otoritas Jasa Keuangan Nomor: S-5/PB.1/2019 tanggal 28 Januari 2019 tentang Rencana Pemisahan dan Pemindahan Alamat Kantor Pusat (KP), Bank memindahkan Kantor
+        Pusatnya yang semula berkedudukan dan berkantor pusat di Denpasar-Balik menjadi berkedudukan dan berkantor pusat di Jakarta Pusat. Perpindaha kantor pusat tersebut kemudian dilaksanakan secara efektif pada tanggal 11 Maret 2019.
+        </small></p>
+
+        <p><small>
+        Perubahan terakhir atas Anggaran Dasar Bank dinyatakan dengan Akta Pernyataan Keputusan Sirkuler Pemegang Saham Perseroan Terbatas PT Bank Mandiri Taspen No. 76 tanggal 30 November 2019 dari Notaris
+        I Gusti Ngurah Putra Wijaya, S.H., Notaris di Denpasar, mengenai penambahan bidang usaha Unit Usaha Syariah. Akta perubahan tersebut telah dilaporkan kepada Menteri Hukum dan Hak Asasi Manusia 
+        Republik INdonesia sebagaimana surat penerimaan pemberitahuan perubahan data Perseroan tertanggal 2 Desember 2019 No. AHU-AH.01.03-0366987 dan telah didaftarkan pada daftar Perseroan Nomor
+        AHU-0231812.AH.01.11 Tahun 2019 tanggal 2 Desember 2019. Perubahan Anggaran Dasar tersebut juga telah disetujui berdasarkan Keputusan Hukum dan Hak Asasi Manusia Republik Indonesia
+        No. AHU-0100178.AH.01.02 Tahun 2019 tentang Persetujuan Perubahan Anggaran Dasar Perseroan Terbatas PT Bank Mandiri Taspen.
+        </small></p>
+
+        <p><small>
+        Bank memperoleh izin usaha sebagai Bank Umum berdasarkan Surat Keputusan Menteri Keuangan Republik Indonesia no. 77/KMK.017/1994 tanggal 10 Maret 1994 dan Surat Keputusan Bank Indonesia 
+        No.27/65/UPBS/PBD2 tanggal 11 Mei 1994 perihal Pemberian Izin Usaha Bank Umum.
+        </small></p>
+
+        <p> <small>
+        Pada tanggal 21 Juni 2017, Bank memperoleh pernyataan efektif dari Ketua OJK dengan suratnya No, S-339/D.04/2017 untuk melakukan penawaran umum Obligasi Bank Mandiri Taspen Pos I tahun 2017
+        dengan jumlah pokok sebesar Rp.2.000.000.000. Pada tanggal 12 Juli 2017, Obligasi tersebut telah dicatat pada Bursa Efek Indonesia. Dan memperoleh pernyataan efektif pada tanggal 18 November 2019 untuk
+        menerbitkan Penawaran Umum Berkelanjutan Obligasi Berkelanjutan I Bank Mandiri Taspen dengan jumlah pokok Rp.1.000.000.000, dan telah pada tanggal 27 November 2019 telah di catat pada Bursa Efek Indonesia.
+        </small></p>
+
+      </div>
+    </div>
+    <div><p align="center"><button class="btn btn-lg">Download Profil Lengkap Tentang Bank Mantap</button></p></div>
+  </div>
+   
+    
 </section>
-
-</div>
 
 
 <?php include 'layout/footer.php';?>
