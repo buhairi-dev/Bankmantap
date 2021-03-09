4500f3c979093a5a147f7a5129019a706f50c906
Adminlocal <buhairi_mail@yahoo.com>
2021-03-09 16:57:22 +0700

    Update Budaya kerja

--- budayakerja.php
+++ budayakerja.php
@@ -12,7 +12,39 @@
 </ul>
 </section>
 
-<div class="container mb-5 text-center">
-    <h1>Budaya Kerja</h1>
-</div>
+<section class="section mb-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <h4 style="color:#0F2B5B;" class="mt-5"><strong>Budaya Kerja Bank Mandiri Taspen</strong></h4>
+        <h4 style="color:#0F2B5B;" class="mt-3">Nilai Budaya Perusahaan TOP</h4>
+        
+        <table class="mt-3" style="font-size:13px;">
+            <tr>
+                <td style="color:#0F2B5B; font: normal normal bold 14px/24px Roboto;">Tangguh</td>
+                <td>: Pantang Menyerah, Adaptif dan Pembelajar</td>
+            
+            </tr>
+            <tr>
+                <td style="color:#0F2B5B; font: normal normal bold 14px/24px Roboto;">Optimal</td>
+                <td>: Memberi hasil yang terbaik</td>
+            </tr>
+            <tr>
+                <td style="color:#0F2B5B; font: normal normal bold 14px/24px Roboto;">Profesional</td>
+                <td>: Kompeten dan dapat dipercaya</td>
+            </tr>
+            <tr>
+                <td style="color:#0F2B5B; font: normal normal bold 14px/24px Roboto;">Peduli</td>
+                <td>: Melayani dengan hati</td>
+            </tr>
+        </table>
+      </div>
+      
+      <div class="col-sm-5">
+        <img src="asset/budayakerja.png"  alt="Logo" width="550px" style="left: 686px; height: 360px; margin-bottom:8%;">
+      </div>
+    </div>
+</section>
+
+<br/><br/><br/><br/>
 <?php include 'layout/footer.php';?>
--- strukturorganisasi.php
+++ strukturorganisasi.php
@@ -12,181 +12,24 @@
 </ul>
 </section>
 
-<style>
+<div class="container">
+<h4 style="color:#0F2B5B;">Penghargaan</h4>
+<p  class="mb-5"><small>Daftar penghargaan yang diperoleh oleh Bank Mantap</small></p>
+</div>
+
+<div class="container mb-5 text-center">
+    <h3 style="color:#0F2B5B; font-weight:bold; padding-bottom:45px;">Struktur Organisasi PT Bank Mandiri Taspen</h3>
+    <img src="asset/strukturorganisasi.png" width="100%">  
+</div>
+<br/> 
+<div class="container mb-5">
+<button class="btn btn-sm mr-1" style="background: #0F2B5B 0% 0% no-repeat padding-box;border-radius: 5px; height: 20px;">&nbsp;</button><small class="mr-3"> Directorat</small>
+<button class="btn btn-sm mr-1" style="background: #FCD116 0% 0% no-repeat padding-box;border-radius: 5px; height: 20px;">&nbsp;</button><small class="mr-3"> SEVP</small>
+<button class="btn btn-sm mr-1" style="background: #94C9EF 0% 0% no-repeat padding-box;border-radius: 5px; height: 20px;">&nbsp;</button><small class="mr-3"> Division</small>
+<button class="btn btn-sm mr-1" style="background: #FFFFFF 0% 0% no-repeat padding-box;border-radius: 5px; height: 20px; border: 1px solid #75B2DD;">&nbsp;</button><small class="mr-3"> Unit/TEam</small>
+</div>
+<br/><br/><br/>
+ <div class="container mb-5 text-center"><img src="asset/strukturusaha.png" width="70%" class="mb-5"></div>
+ <br/><br/><br/>
   
-    .nav-item a{
-        color:#0F2B5B;
-        
-    }
-</style>
-
-<section class="section" style="margin-top:-2%; margin-bottom:9%;">
-  <div class="container">
-    <div class="row center">
-        <ul class="nav nav-pills mb-5" id="pills-tab" role="tablist">
-            <li class="nav-item">
-              <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Dewan Komisaris</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Direksi</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Sekretaris Perusahaan</a>
-            </li>
-          </ul>
-          <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="card" style="width: 20rem; box-shadow: 0px 20px 40px #00000014; border-radius: 16px;">
-                            <img class="card-img-top" src="asset/dewankomisaris_01.png" alt="Card image cap">
-                            <div class="card-body" style="text-align: center;">
-                            <p class="card-text" style="font-size: 20px; font-weight: 500;">Abdul Rahman</p>
-                            <p class="card-text" style="font-size: 12px;">Komisaris Utama</p>
-                            <p class="card-text"><a href="" data-toggle="modal" data-target="#myModal">Lihat Profil</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card" style="width: 20rem; box-shadow: 0px 20px 40px #00000014; border-radius: 16px;">
-                            <img class="card-img-top" src="asset/dewankomisaris_01.png" alt="Card image cap">
-                            <div class="card-body" style="text-align: center;">
-                            <p class="card-text" style="font-size: 20px; font-weight: 500;">Zudan Arif Fakhrullah</p>
-                            <p class="card-text" style="font-size: 12px;">Komisaris Independen *)</p>
-                            <p class="card-text"><a href="#">Lihat Profil</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card" style="width: 20rem; box-shadow: 0px 20px 40px #00000014; border-radius: 16px;">
-                            <img class="card-img-top" src="asset/dewankomisaris_01.png" alt="Card image cap">
-                            <div class="card-body" style="text-align: center;">
-                            <p class="card-text" style="font-size: 20px; font-weight: 500;">Abdul Rahman</p>
-                            <p class="card-text" style="font-size: 12px;">Komisaris Independen</p>
-                            <p class="card-text"><a href="#">Lihat Profil</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                 
-                <div class="row mt-5">
-                    <div class="col-lg-4">
-                        <div class="card" style="width: 20rem; box-shadow: 0px 20px 40px #00000014; border-radius: 16px;">
-                            <img class="card-img-top" src="asset/dewankomisaris_01.png" alt="Card image cap">
-                            <div class="card-body" style="text-align: center;">
-                            <p class="card-text" style="font-size: 20px; font-weight: 500;">Andi Rivai</p>
-                            <p class="card-text" style="font-size: 12px;">Komisaris Independen *)</p>
-                            <p class="card-text"><a href="#">Lihat Profil</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="card" style="width: 20rem; box-shadow: 0px 20px 40px #00000014; border-radius: 16px;">
-                            <img class="card-img-top" src="asset/dewankomisaris_01.png" alt="Card image cap">
-                            <div class="card-body" style="text-align: center;">
-                            <p class="card-text" style="font-size: 20px; font-weight: 500;">Abdul Rahman</p>
-                            <p class="card-text" style="font-size: 12px;">Komisaris Utama</p>
-                            <p class="card-text"><a href="#">Lihat Profil</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card" style="width: 20rem; box-shadow: 0px 20px 40px #00000014;  border-radius: 16px;">
-                            <img class="card-img-top" src="asset/dewankomisaris_01.png" alt="Card image cap">
-                            <div class="card-body" style="text-align: center;">
-                            <p class="card-text" style="font-size: 20px; font-weight: 500;">Zudan Arif Fakhrullah</p>
-                            <p class="card-text" style="font-size: 12px;">Komisaris Independen *)</p>
-                            <p class="card-text"><a href="#">Lihat Profil</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card" style="width: 20rem; box-shadow: 0px 20px 40px #00000014;  border-radius: 16px;">
-                            <img class="card-img-top" src="asset/dewankomisaris_01.png" alt="Card image cap">
-                            <div class="card-body" style="text-align: center;">
-                            <p class="card-text" style="font-size: 20px; font-weight: 500;">Abdul Rahman</p>
-                            <p class="card-text" style="font-size: 12px;">Komisaris Independen</p>
-                            <p class="card-text"><a href="#">Lihat Profil</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-               
-                <div class="row mt-5">
-                    <div class="col-lg-4">
-                        <div class="card" style="width: 20rem; box-shadow: 0px 20px 40px #00000014;  border-radius: 16px;">
-                            <img class="card-img-top" src="asset/dewankomisaris_01.png" alt="Card image cap">
-                            <div class="card-body" style="text-align: center;">
-                            <p class="card-text" style="font-size: 20px; font-weight: 500;">Andi Rivai</p>
-                            <p class="card-text" style="font-size: 12px;">Komisaris Independen *)</p>
-                            <p class="card-text"><a href="#">Lihat Profil</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab"></div>
-          </div>
-    </div>
-  </div>
-   
-    
-
- 
-    <style>
-        .modal-dialog{
-            width: 729px;
-            height: 1085px;
-            background: #FFFFFF 0% 0% no-repeat padding-box;
-            border-radius: 16px;
-            opacity: 1;
-        }
-
-        .modal-content{
-            background: var(--yellow-fcd116) 0% 0% no-repeat padding-box;
-            background: #FCD116 0% 0% no-repeat padding-box;
-            border-radius: 16px 16px 0px 0px;
-            opacity: 1;
-        }
-    </style>
-        <div class="modal fade" id="myModal" role="dialog">
-        <div class="modal-dialog modal-lg">
-
-            <!-- Modal content-->
-            <div class="modal-content">
-            <div class="row">
-                <div class="col-sm">
-                <img class="card-img-top" src="asset/dewankomisaris_01.png">
-                </div>
-                <div class="col-sm">
-                   <h3>Abdul Rahman</h3>
-                   <p>Komisaris Utama</p>
-                </div>
-            </div>
-            </div>
-            
-            <div class="row mt-5" sytle="font-size:10px;">
-            <div class="col-sm-3 ml-4 text-left" style="color: #0F2B5B;">
-                <p>Umur</p>
-                <p>Warga Negara</p>
-                <p>Domisili</p>
-                <p>Pendidikan</p>
-                
-            </div>
-            <div class="col-sm-3-right" style="width:70%;">
-            <p>52 Tahun</p>
-            <p>Indonesia</p>
-            <p>Jakarta</p>
-            <p>Sarjana Keuangan dan Akuntansi (jurusan ganda) dari University of Virginia, Charlottesville, Amerika Serikat (1995)</p>
-          
-            </div>
-            
-        </div>
-            <!-- /.modal-content -->
-    
-</section>
 <?php include 'layout/footer.php';?>
