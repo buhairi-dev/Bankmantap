1a62e1e9fd8e39013116d3ceab9afc6695ddff99
Adminlocal <buhairi_mail@yahoo.com>
2021-03-09 10:15:46 +0700

    Update Penghargaan

---
+++ penghargaan.php
@@ -0,0 +1,123 @@
+<?php include 'layout/header.php';?>
+ 
+
+<section id="img-header" class="img-header">
+<img src="asset/slider3.png">
+<ul class="breadcrumb">
+<div class="container">
+  <li><a href="#">Home</a></li>
+  <li>Tentang Kami</li>
+  <li>Penghargaan</li>
+</div>
+</ul>
+</section>
+
+<div class="container">
+<h4 style="color:#0F2B5B;">Penghargaan</h4>
+<p  class="mb-5"><small>Daftar penghargaan yang diperoleh oleh Bank Mantap</small></p>
+<style>
+table {
+  margin-bottom:25%;
+  width: 100%;
+  font-size:12px;
+  box-shadow: 0px 20px 40px #00000014;
+  border-radius: 16px;
+   
+}
+
+th {
+   
+  height: 60px;
+  color: #121212;
+
+}
+td {
+  text-align: left;
+  height: 40px;
+  color: #121212;;
+}
+tr:nth-child(even){
+    background-color: #FCD1161A;
+}
+
+th {
+  background-color: #FCD116;
+  color: #0F2B5B;
+}
+
+tr .footer{
+  font-weight:bold;
+  background-color: #D0D8E6;
+  height: 50px;
+  color: #0F2B5B;
+  
+}
+</style>
+<table>
+<thead>
+  <tr>
+    <th class="pl-4" style="border-radius: 16px 0px 0px 0px;" colspan="2">Tahun 2000</th>
+    <th style="border-radius: 0px 16px 0px 0px;">Diskripsi</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="text-right" style="width:4%">1.</td>
+    <td style="width:25%;">Infobank Award</td>
+    <td>: The Best SME Business Performance Year 2019 “Bank BUKU II dengan Asset Rp 10T s.d Rp 25T</td>
+  </tr>
+  
+  <tr>
+    <td  class="text-right">2.</td>
+    <td>Warta Ekonomi Award</td>
+    <td>: Best Bank Award 2020 “Survive The Inevitable Future” & “Bank Berpredikat Sangat Sehat Pada Kategori Buku II dengan aset diatas Rp 20T</td>
+  </tr>
+  <tr>
+    <td  class="text-right">3.</td>
+    <td>Investor Award 2020</td>
+    <td>: Bank Terbaik 2020 Kategori Bank Umum Dengan Modal Inti Rp 1T - Rp 5T</td>
+  </tr>
+  
+  <tr>
+    <td  class="text-right">4.</td>
+    <td>Infobank Award 2020</td>
+    <td>: Golden Throphy “Sangat Bagus 5 Tahun Berturut-turut” & Bank Terbaik Dengan Modal Inti Rp 1T s.d Rp 5T</td>
+  </tr>
+  
+  <tr>
+    <td  class="text-right">5.</td>
+    <td>Bank Indonesia Award</td>
+    <td>: Bank Pendukung UMKM Terbaik – Bank Buku 1 & 2</td>
+  </tr>
+
+  <tr>
+    <td  class="text-right">6.</td>
+    <td>Bisnis Indonesia Financial Award 2020</td>
+    <td>: Bank Swasta Non Devisa Terbaik</td>
+  </tr>
+
+  <tr>
+    <td  class="text-right">7.</td>
+    <td>Infobank Award 2020</td>
+    <td>: Golden Throphy “Sangat Bagus 5 Tahun Berturut-turut” & Bank Terbaik Dengan Modal Inti Rp 1T s.d Rp 5T</td>
+  </tr>
+
+  <tr>
+    <td  class="text-right">8.</td>
+    <td>Marketers Editor’s Choice Award 2020</td>
+    <td>: Community Empowerment Of The Year</td>
+  </tr>
+  
+  <tr>
+    <td  class="text-right" style="border-radius: 0px 0px 0px 16px;">9.</td>
+    <td>Warta Ekonomi Award</td>
+    <td  style="border-radius: 0px 0px 16px 0px;">: Best CEO Bank 2020</td>
+  </tr>
+
+</tbody>
+
+   
+
+</table>
+</div>
+<?php include 'layout/footer.php';?>
