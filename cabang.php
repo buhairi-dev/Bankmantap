76eb02b79b0ad141417cb13f79df58c39c8ea5e8
Adminlocal <buhairi_mail@yahoo.com>
2021-03-16 02:19:13 +0700

    Update All

---
+++ cabang.php
@@ -0,0 +1,168 @@
+<?php include 'layout/header.php';?>
+
+<section id="img-header" class="img-header">
+<div>
+     
+        <div class="row">
+            &nbsp
+            <div id="googlemap" style="width: 100%; height: 400px;"></div>
+        </div>
+ 
+</div>
+
+<ul class="breadcrumb">
+
+<div class="container">
+  <li><a href="#">Home</a></li>
+  <li>Kantor Cabang</li>
+</div>
+
+</ul>
+</section>
+
+<div class="container" id="search">
+    <h4><b>Lokasi Kantor Cabang Mandiri Taspen</b></h4>
+        <div class="row mt-4">
+          <div class="col-sm text-left">
+            <form action="home.php">
+            <select class="btn btn-lg" aria-label="Default select example">
+              <option selected>Pilih area kantor cabang (Provinsi)</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class="col-sm text-center"><button class="btn btn-lg btn-search" type="submit">Cari</button></div>
+        </form>
+        </div>
+</div>
+
+ 
+<div class="container mt-5">
+
+  <div   class="row mb-5">
+
+    <div class="col-sm">
+      <div class="card branch">
+        <div class="card-body">
+        <br/>
+        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+        <p><small>Wisma Mandiri</small><br/>
+        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
+        <small>021-230265</small></p>
+        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;<a href="">Lihat Map</a></h3>
+        </div>
+      </div>
+		</div>
+
+    <div class="col-sm">
+      <div class="card branch">
+        <div class="card-body">
+        <br/>
+        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+        <p><small>Wisma Mandiri</small><br/>
+        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
+        <small>021-230265</small></p>
+        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
+        </div>
+      </div>
+		</div>
+
+    <div class="col-sm">
+      <div class="card branch">
+        <div class="card-body">
+        <br/>
+        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+        <p><small>Wisma Mandiri</small><br/>
+        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
+        <small>021-230265</small></p>
+        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
+        </div>
+      </div>
+		</div>
+
+    <div class="col-sm">
+      <div class="card branch">
+        <div class="card-body">
+        <br/>
+        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+        <p><small>Wisma Mandiri</small><br/>
+        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
+        <small>021-230265</small></p>
+        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
+        </div>
+      </div>
+		</div>
+
+ 
+
+  </div> <!-- End Row -->
+
+
+  <div   class="row mb-5">
+
+    <div class="col-sm">
+      <div class="card branch">
+        <div class="card-body">
+        <br/>
+        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+        <p><small>Wisma Mandiri</small><br/>
+        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
+        <small>021-230265</small></p>
+        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
+        </div>
+      </div>
+		</div>
+
+    <div class="col-sm">
+      <div class="card branch">
+        <div class="card-body">
+        <br/>
+        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+        <p><small>Wisma Mandiri</small><br/>
+        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
+        <small>021-230265</small></p>
+        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
+        </div>
+      </div>
+		</div>
+
+    <div class="col-sm">
+      <div class="card branch">
+        <div class="card-body">
+        <br/>
+        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+        <p><small>Wisma Mandiri</small><br/>
+        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
+        <small>021-230265</small></p>
+        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
+        </div>
+      </div>
+		</div>
+
+    <div class="col-sm">
+      <div class="card branch">
+        <div class="card-body">
+        <br/>
+        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
+        <p><small>Wisma Mandiri</small><br/>
+        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
+        <small>021-230265</small></p>
+        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
+        </div>
+      </div>
+		</div>
+
+ 
+
+  </div>  <!-- End Row -->
+
+</div>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+  
+<?php include 'layout/footer.php';?>
