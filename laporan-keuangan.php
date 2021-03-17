2333342f18d49b65c27b5584e6f53f07a220c383
Adminlocal <buhairi_mail@yahoo.com>
2021-03-17 08:34:53 +0700

    Update All

--- laporan-keuangan.php
+++ laporan-keuangan.php
@@ -1,20 +1,5 @@
 <?php include 'layout/header.php';?>
-<style>
-.active,.btn-wr:hover{
-	background-color: #FCD116;
-	color:black;
-	color: #121212;
-	opacity: 1;
-	font-size: 13px;
-	border-radius: 25px;
-	width: 179px;
-	font-family: 'Roboto', sans-serif;
-	font-weight: 900;
-	border:none;
-	transition-duration: 0.7s;
-	box-shadow: 0 8px 10px 0 rgba(0,0,0,0.24), 0 14px 40px 0 rgba(0,0,0,0.15);
- }
-</style>
+ 
 <section id="img-header" class="img-header">
 <img src="asset/slider20.png">
  
@@ -37,10 +22,137 @@
 <p><a class="btn btn-lg btn-wr" href="#" role="button">Berita Mantap</a>
 <a class="btn btn-lg btn-wr" href="#" role="button">Promosi Mantap</a>
 <a class="btn btn-lg btn-wr" href="#" role="button">Program Mantap</a>
-<a class="btn btn-lg btn-wr active" href="#" role="button">Laporan Keuangan</a></p>
- 
+<a class="btn btn-lg btn-wr" href="#" role="button">Laporan Keuangan</a></p>
+
+
 
 </div> <!-- container -->
+</section>
+
+<div class="container">
+<h4><strong>Laporan Tahunan</strong></h4>
+</div>
+<div class="container" style="margin-top:25%">
+
+<div class="container my-3">
+  <!--Carousel Wrapper-->
+  <div id="news" class="carousel slide carousel-multi-item" data-ride="carousel" style="margin-top: -20rem;">
+    <ol class="carousel-indicators" style="top: 25rem;">
+      <li data-target="#news" data-slide-to="0" class="active"  style="width: 8%; height: 7px; background: var(--yellow-fcd116) 0% 0% no-repeat padding-box;
+      background: #FCD116 0% 0% no-repeat padding-box; border-radius: 6px;"></li>
+      <li data-target="#news" data-slide-to="1"  style="width: 6px; height: 6px; padding-box; border-radius: 6px; background-color: #121212;"></li>
+      <li data-target="#news" data-slide-to="2"  style="width: 6px; height: 6px; padding-box; border-radius: 6px; background-color: #121212;"></li>
+      <li data-target="#news" data-slide-to="3"  style="width: 6px; height: 6px; padding-box; border-radius: 6px; background-color: #121212;"></li>
+    </ol>
+  <!--Slides-->
+  
+  <div class="carousel-inner" role="listbox">
+    <!--First slide-->
+    <div class="carousel-item active">
+        
+      <div class="col-md-3" style="float:left;">
+       <div class="card mb-2" style="height: 23rem; box-shadow: 0px 20px 40px #75B2DD1A;border-radius: 12px 12px 0px 0px; opacity: 1;">
+          <img class="card-img-top" src="asset/minislide1.png" alt="Card image cap"  style="height: 12rem;">
+          <div class="card-body">
+          <p class="card-text text-left"><small class="text-muted">Last Update : 7 Des 2020</small></p>
+          <p class="card-text text-left" style="color: #0F2B5B; cursor:pointer;" onclick="location.href='404.html';">Bank Mantap Serahkan Bantuan Alat Pelindung Diri Kepada RSPI Sulianti Saroso</p>
+          </div>
+        </div>
+      </div>
+  
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2" style="height: 23rem; box-shadow: 0px 20px 40px #75B2DD1A;border-radius: 12px 12px 0px 0px; opacity: 1;">
+          <img class="card-img-top" src="asset/minislide2.png" alt="Card image cap"  style="height: 12rem;">
+          <div class="card-body">
+          <p class="card-text text-left"><small class="text-muted">Last Update : 16 Nov 2020</small></p>
+          <p class="card-text text-left" style="color:#0F2B5B; cursor:pointer;" onclick="location.href='404.html';">Hadiah langsung tabungan siMantap, tentukan hadiahmu!</p>   
+          </div>
+        </div>
+      </div>
+      
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2" style="height: 23rem; box-shadow: 0px 20px 40px #75B2DD1A;border-radius: 12px 12px 0px 0px; opacity: 1;">
+          <img class="card-img-top" src="asset/minislide3.png" alt="Card image cap" style="height: 12rem;">
+          <div class="card-body">
+          <p class="card-text text-left"><small class="text-muted">Last Update : 10 Nov 2020</small></p>
+          <p class="card-text text-left" style="color: #0F2B5B; cursor:pointer;" onclick="location.href='404.html';">Pegawai Bank Mandiri Taspen Sisihkan Gaji Untuk Bantuan Akibat Dampak Virus Covid-19</p>
+          </div>
+        </div>
+      </div>
+      
+       <div class="col-md-3" style="float:left">
+       <div class="card mb-2" style="height: 23rem; box-shadow: 0px 20px 40px #75B2DD1A;border-radius: 12px 12px 0px 0px; opacity: 1;">
+          <img class="card-img-top" src="asset/minislide4.png" alt="Card image cap" style="height: 12rem;">
+          <div class="card-body">
+          <p class="card-text text-left"><small class="text-muted">Last Update : 25 Okt 2020</small></p>
+          <p class="card-text text-left" style="color: #0F2B5B; cursor:pointer;" onclick="location.href='404.html';">Badan Kepegawaian Negara Taspen dan Bank Mantap Perkuat Sinergi</p>
+          </div>
+        </div>
+      </div>
+  
+    </div>
+    <!--/.First slide-->
+    <div class="carousel-item">
+        
+      <div class="col-md-3" style="float:left">
+       <div class="card mb-2" style="height: 23rem; box-shadow: 0px 20px 40px #75B2DD1A;border-radius: 12px 12px 0px 0px; opacity: 1;">
+          <img class="card-img-top" src="asset/minislide1.png" alt="Card image cap" style="height: 12rem;">
+          <div class="card-body">
+          <p class="card-text text-left"><small class="text-muted">Last Update : 7 Des 2020</small></p>
+          <p class="card-text text-left" style="color: #0F2B5B; cursor:pointer;" onclick="location.href='404.html';">Bank Mantap Serahkan Bantuan Alat Pelindung Diri Kepada RSPI Sulianti Saroso</p>
+          </div>
+        </div>
+      </div>
+  
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2" style="height: 23rem; box-shadow: 0px 20px 40px #75B2DD1A;border-radius: 12px 12px 0px 0px; opacity: 1;">
+          <img class="card-img-top" src="asset/minislide2.png" alt="Card image cap" style="height: 12rem;">
+          <div class="card-body">
+          <p class="card-text text-left"><small class="text-muted">Last Update : 16 Nov 2020</small></p>
+          <p class="card-text text-left" style="color: #0F2B5B; cursor:pointer;" onclick="location.href='404.html';">Hadiah langsung tabungan siMantap, tentukan hadiahmu!</p>   
+          </div>
+        </div>
+      </div>
+  
+      <div class="col-md-3" style="float:left">
+        <div class="card mb-2" style="height: 23rem; box-shadow: 0px 20px 40px #75B2DD1A;border-radius: 12px 12px 0px 0px; opacity: 1;">
+          <img class="card-img-top" src="asset/minislide3.png" alt="Card image cap" style="height: 12rem;">
+          <div class="card-body">
+          <p class="card-text text-left"><small class="text-muted">Last Update : 10 Nov 2020</small></p>
+          <p class="card-text text-left" style="color: #0F2B5B; cursor:pointer;" onclick="location.href='404.html';">Pegawai Bank Mandiri Taspen Sisihkan Gaji Untuk Bantuan Akibat Dampak Virus Covid-19</p>
+          </div>
+        </div>
+      </div>
+      
+       <div class="col-md-3" style="float:left">
+       <div class="card mb-2" style="height: 23rem; box-shadow: 0px 20px 40px #75B2DD1A;border-radius: 12px 12px 0px 0px; opacity: 1;">
+          <img class="card-img-top" src="asset/minislide4.png" alt="Card image cap" style="height: 12rem;">
+          <div class="card-body">
+          <p class="card-text text-left"><small class="text-muted">Last Update : 25 Okt 2020</small></p>
+          <p class="card-text text-left" style="color: #0F2B5B; cursor:pointer;" onclick="location.href='404.html';">Badan Kepegawaian Negara Taspen dan Bank Mantap Perkuat Sinergi</p>
+          </div>
+        </div>
+      </div>
+  
+    </div>
+    <!--Second slide--> 
+  
+    
+    <!--/.Second slide-->
+  
+  
+  </div>
+  <!--/.Slides-->
+
+  <!-- Indicator Slider -->
+  <a class="carousel-control-prev" href="#news" role="button" data-slide="prev"><img src="asset/left.svg" style="margin-left: -10rem;"></a>
+  <a class="carousel-control-next" href="#news" role="button" data-slide="next"><img src="asset/right.svg" style="margin-right: -10rem;"></a>
+   <!-- End Indicator Slider -->
+    
+  </div>
+</div>
+ 
+
 
 
 <div class="container paging mt-5" id="page">
