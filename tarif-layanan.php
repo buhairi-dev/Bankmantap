0fa72680be9d0b01976744ac7ab899d5f61bc541
Adminlocal <buhairi_mail@yahoo.com>
2021-03-17 17:35:31 +0700

    Update All

--- tarif-layanan.php
+++ tarif-layanan.php
@@ -8,7 +8,6 @@ table {
   font-size:12px;
   box-shadow: 0px 20px 40px #00000014;
   border-radius: 16px;
-   
 }
 
 th {
@@ -19,7 +18,12 @@ th {
 td {
   text-align: left;
   height: 40px;
-  color: #121212;;
+  color: #121212;
+  background-color: #fff;
+  border-top-style: solid;
+  border-top-width: 1px;
+ 
+  border-color:#D0D8E6;
 }
 tr:nth-child(even){
     background-color: #FCD1161A;
@@ -30,13 +34,6 @@ th {
   color: #0F2B5B;
 }
 
-tr .footer{
-  font-weight:bold;
-  background-color: #D0D8E6;
-  height: 50px;
-  color: #0F2B5B;
-  
-}
 </style>
 
 
@@ -69,8 +66,8 @@ tr .footer{
 </thead>
 <tbody>
   <tr>
-    <td class="pl-5">1</td>
-    <td colspan="2">Giro Rupiah</td>
+    <td class="pl-5" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">1</td>
+    <td colspan="2" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">Giro Rupiah</td>
   </tr>
    
   <tr>
@@ -80,8 +77,8 @@ tr .footer{
   </tr>
 
   <tr>
-    <td class="pl-5"></td>
-    <td><strong>Biaya Administrasi Badan Usaha</strong></td>
+    <td class="pl-5" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;"></td>
+    <td colspan="2" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;"><strong>Biaya Administrasi Badan Usaha</strong></td>
   </tr>
 
   <tr>
@@ -109,8 +106,8 @@ tr .footer{
   </tr>
 
   <tr>
-    <td class="pl-5"></td>
-    <td colspan="2"><strong>Cek atau Bilyet Giro Hilang</strong></td>
+    <td class="pl-5" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;"></td>
+    <td colspan="2" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;"><strong>Cek atau Bilyet Giro Hilang</strong></td>
   </tr>
 
   <tr>
@@ -138,17 +135,342 @@ tr .footer{
   </tr>
 
   <tr>
+    <td class="pl-5" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;"></td>
+    <td colspan="2" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;"><strong>Biaya Penutupan</strong></td>
+  </tr>
+
+  <tr>
     <td class="pl-5"></td>
-    <td colspan="2"><strong>Biaya Penutupan</strong></td>
+    <td colspan="2">a. Oleh Nasabah</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>- Perorangan</td>
+    <td>Rp 50,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>- Institusi</td>
+    <td>Rp 100,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>b. Oleh Bank</td>
+    <td>Rp 25,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>c. Biaya permintaan rekening koran diluar R/K bulanan</td>
+    <td>Rp 3,000,-/lembar</td>
+</tr>
+  
+  <tr>
+    <td class="pl-5"></td>
+    <td>d. Biaya pembelian Buku Cek / Bilyet Giro</td>
+    <td>Rp 125,000,-/buku</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">2</td>
+    <td colspan="2" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">Tabungan siMantap Gold</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>a. Biaya Administrasi</td>
+    <td>Rp 6,500,-/bulan</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>b. Biaya Penutupan Rekening</td>
+    <td>Rp 25,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>c. Biaya rekening pasif / dormant</td>
+    <td>Rp 2,000,-/bulan</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td colspan="2">d. Penggantian buku karena penuh</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>- Cabang Pengelola</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>- Cabang Lain</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>- Cabang Lain</td>
+    <td>Rp 5,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>e. Penggantian buku karena rusak</td>
+    <td>Rp 5,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>f. Penggantian buku karena hilang</td>
+    <td>Rp 15,000,-</td>
+  </tr>
+  <tr>
+    <td class="pl-5"></td>
+    <td>g. Setoran Beda Cabang (Tujuan Luar Kota)</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">3</td>
+    <td colspan="2" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">Tabungan siMantap Berjangka</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>a. Administrasi per bulan</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>b. Biaya penutupan rekening pada saat jatuh tempo</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>C. Biaya penarikan pada masa perjanjian</td>
+    <td>Rp 100,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>d. Biaya penutupan rekening sebelum jatuh tempo - Adm + penalty</td>
+    <td>Rp 105,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>e. Biaya penalty menunggak 1 bulan setoran wajib bulanan pada saat jatuh tempo</td>
+    <td>Rp 10,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>f. Biaya penalty menunggak 2 bulan setoran wajib bulanan pada saat jatuh tempo</td>
+    <td>Rp 20,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>g. Biaya penutupan rekening apabila klaim ditolak</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>h. Biaya permintaan rekening koran (R/K) di luar (R/K) yang disampaikan Bank secara triwulan</td>
+    <td>Rp 3,000,-/lembar</td>
   </tr>
 
 
 
+   <tr>
+    <td class="pl-5" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">4</td>
+    <td colspan="2" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">Tabunganku</td>
+  </tr>
+
 
   <tr>
-    <td class="pl-4" style="border-radius: 16px 0px 0px 0px;"></td>
+    <td class="pl-5"></td>
+    <td>a. Biaya administrasi</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>b. Biaya penutupan rekening</td>
+    <td>Rp 20,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>c. Biaya rekening pasif / dormant</td>
+    <td>Rp 2,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td colspan="2">d. Penggantian buku karena penuh</td>
+     
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>- Cabang Pengelola</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>- Cabang Lain</td>
+    <td>Rp 5,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>e. Penggantian buku karena rusak</td>
+    <td>Rp 5,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>f. Biaya penarikan tunai dan non tunai di cabang lain</td>
+    <td>Rp 15,000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>g. Biaya administrasi bulanan kartu Tabungaku</td>
+    <td>Rp 5,000,-</td>
+  </tr>
+
+
+  <tr>
+    <td class="pl-5" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">5</td>
+    <td colspan="2" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">Deposito</td>
+  </tr>
+
+ 
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>a. Penerbitan pencairan</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>b. Penalty pencairan sebelum jatuh tempo</td>
+    <td>5% dari nominal</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">6</td>
+    <td colspan="2" style="background-color: #FCD1161A; color:#0F2B5B; font-weight:bold;">Produk Kartu ATM</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>a. Administrasi ATM Plus</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>b. Administrasi ATM Reguler</td>
+    <td>5% dari nominal</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>c. Administrasi ATM Tabunganku</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>d. Penggantian kartu karena rusak (akibat kelalaian nasabah)</td>
+    <td>5% dari nominal</td>
+  </tr>
+
+ 
+  <tr>
+    <td class="pl-5"></td>
+    <td>e. Penggantian kartu karena hilang</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>f. Permohonan Reissue PIN ATM</td>
+    <td>5% dari nominal</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>g. Biaya informasi saldo melalui ATM Bank Mandiri</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>h. Biaya informasi saldo melalui ATM Bersama & LINK</td>
+    <td>5% dari nominal</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>i. Biaya penarikan tunai melalui ATM Bank Mandiri</td>
+    <td>5% dari nominal</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>j. Transfer melalui ATM Bank Mandiri - Rek Bank Mantap ke Rek Bank Mantap</td>
+    <td>Bebas</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>k. Transfer melalui ATM Bank Mandiri - Rek Bank Mantap ke Rek Bank Mandiri</td>
+    <td>5% dari nominal</td>
+  </tr>
+
+  <tr>
+    <td class="pl-5"></td>
+    <td>l. Maksimal Penarikan dan Transfer ATM (Plus, Reguler, dan Tabunganku)</td>
+    <td>Maks. Penarikan ATM Plus Rp. 10.000.000,-</td>
+  </tr>
+
+  <tr>
+    <td colspan="2"></td>
+    <td>Maks. Penarikan ATM Plus Rp. 20.000.000,-</td>
+  </tr>
+  <tr>
+    <td colspan="2"></td>
+    <td>Maks. Penarikan ATM Reguler Rp. 5.000.000,-</td>
+  </tr>
+  <tr>
+    <td colspan="2"></td>
+    <td>Mak. Penarikan ATM Tabunganku Rp. 3.000.000,-</td>
+  </tr>
+  <tr>
+    <td colspan="2"></td>
+    <td>Maks. Transfer ATM Tabunganku Rp. 5.000.000,-</td>
+  </tr>
+
+  <tr>
+    <td class="pl-4" style="border-radius: 0px 0px 0px 16px;"></td>
     <td></td>
-    <td class="pl-4" style="border-radius: 0px 16px 0px 0px;"></td>
+    <td class="pl-4" style="border-radius: 0px 0px 16px 0px;"></td>
   </tr>
 </tbody>
 
