b42aa1c997c7204829f8ef84cabb198d1ab4bd1a
Adminlocal <buhairi_mail@yahoo.com>
2021-03-17 08:03:19 +0700

    Update All

--- bank-garansi.php
+++ bank-garansi.php
@@ -15,7 +15,44 @@
 
 <section class="section">
 <!-- Content Start Here -->
+<div class="container">
+    <div class="row">
 
+      <div class="col-sm-6">
+        <h4 class="mt-5"><strong>Bank Garansi</strong></h4>
+        <small>Salah satu hal penting dalam bisnis adalah kepercayaan. Namun, kepercayaan biasanya baru datang bila rekam jejak para pihak yang bertransaksi dapat diketahui atau bila ada pihak yang didukung atau dijamin oleh pihak ketiga yang dapat dipercaya. Peran dari pihak ketiga sebagai penjamin ini umumnya dijalankan oleh Bank dengan menerbitkan Bank Garansi, yaitu janji bank untuk membayar bila ada pihak yang cedera janji.</small><br/>
+
+        <p class="mt-3"><small>Bank Garansi yang diterbitkan Bank Mantap layak menjadi andalan Anda untuk membangun kepercayaan demi kelancaran transaksi bisnis. Bank Mantap siap memberikan layanan Bank Garansi terbaik bagi Anda.</small></p><br/>
+        <p class="mt-3"><small>Adapun hal - hal yang harus diperhatikan apabila Anda menjadi pemohon atau penerima Bank Garansi adalah :</small></p>
+         
+      </div>
+      
+      <div class="col-sm-5">
+        <img src="asset/pic-garansi.png"  alt="Logo" width="550px" style="left: 686px; height: 360px; margin-bottom:8%;">
+      </div>
+
+      <div class="col-sm-12">
+      <h4 class="mt-4">Bagi Pemohon Bank Garansi</h4>
+        <small>Anda dapat mengajukan permohonan penerbitan Bank Garansi dengan cara mendatangi langsung Kantor Cabang Bank Mantap. Bank Mantap tidak melakukan kerja sama dengan pihak lain (seperti broker) dalam penerbitan Bank Garansi.</small><br/>
+      </div>
+ 
+      <div class="col-sm-12">
+      <h4 class="mt-4">Bagi Penerima Bank Garansi</h4>
+        <small>Anda dapat mengkonfirmasikan Bank Garansi yang diterima kepada cabang penerbit Bank Garansi yang tercantum pada warkat Bank Garansi sebelum melakukan tindakan lebih lanjut. Konfirmasi dapat dilakukan dengan cara mendatangi cabang penerbit Bank Garansi dengan membawa Bank Garansi asli atau mengajukan permintan tertulis dengan melampirkan copy Bank Garansi yang diterima.</small><br/>
+      </div>
+ 
+ 
+      <div class="col-sm-12">
+      <h4 class="mt-4">Manfaat</h4>
+        <small>1.&nbsp;&nbsp;&nbsp;Mengurangi risiko cedera janji bagi penerima Bank Garansi</small><br/>
+        <small>2.&nbsp;&nbsp;&nbsp;Melancarkan transaksi bisnis</small><br/>
+        
+        <p class="mt-3"><small>Untuk keterangan lebih lanjut hubungi kantor cabang Kami.</small></p>
+        <br/><br/><br/><br/><br/>
+      </div>
+      
+    </div>
+</div>
 
 
 
