c17b71b22e01f6a40758b0293d35e16eed71be30
Adminlocal <buhairi_mail@yahoo.com>
2021-03-17 07:48:12 +0700

    Update All

--- referensi-bank.php
+++ referensi-bank.php
@@ -15,6 +15,49 @@
 
 <section class="section">
 <!-- Content Start Here -->
+<div class="container">
+    <div class="row">
+
+      <div class="col-sm-6">
+        <h4 class="mt-5"><strong>Referensi Bank</strong></h4>
+        <small>Salah satu hal penting dalam bisnis adalah kepercayaan. Namun, kepercayaan biasanya baru datang bila rekam jejak para pihak yang bertransaksi dapat diketahui atau bila ada pihak yang didukung oleh pihak ketiga yang dapat dipercaya. Peran dari pihak ketiga ini umumnya dijalankan oleh Bank dengan menerbitkan Referensi Bank, yaitu surat keterangan yang diterbitkan oleh Bank Mantap atas dasar permintaan nasabah perorangan maupun perusahaan untuk tujuan tertentu yang bersifat tidak mengikat, tidak menjanjikan dan tidak memberikan jaminan.</small>
+        <p class="mt-3"><small>Referensi bank yang diterbitkan Bank Mantap layak menjadi andalan Anda untuk membangun kepercayaan demi kelancaran transaksi bisnis.</small></p>
+      </div>
+      
+      <div class="col-sm-5">
+        <img src="asset/pic-ref.png"  alt="Logo" width="550px" style="left: 686px; height: 360px; margin-bottom:8%;">
+      </div>
+
+      <div class="col-sm-12">
+      <h4>Karakteristik</h4>
+        <small>1.&nbsp;&nbsp;&nbsp;Khusus diberikan untuk nasabah Bank Mantap</small><br/>
+        <small>2.&nbsp;&nbsp;&nbsp;Surat referensi tidak mengikat Bank Mantap</small>
+      </div>
+
+      <div class="col-sm-12">
+      <h4 class="mt-4">Manfaat</h4>
+        <small>1.&nbsp;&nbsp;&nbsp;Sebagai referensi bahwa nasabah dikenal baik oleh bank (bonafitas nasabah)</small><br/>
+        <small>2.&nbsp;&nbsp;&nbsp;Memenuhi salah satu persyaratan bagi nasabah yang akan melakukan suatu pengajuan pada pihak ketiga.</small>
+      </div>
+
+      <div class="col-sm-12">
+      <h4 class="mt-4">Peruntukkan</h4>
+        <small>1.&nbsp;&nbsp;&nbsp;Perorangan</small><br/>
+        <small>2.&nbsp;&nbsp;&nbsp;Badan Usaha/badan hukum.</small>
+      </div>
+
+      <div class="col-sm-12">
+      <h4 class="mt-4">Syarat</h4>
+        <small>1.&nbsp;&nbsp;&nbsp;Memiliki rekening di Bank Mantap</small><br/>
+        <small>2.&nbsp;&nbsp;&nbsp;Membuat surat permohonan pembuatan referensi bank.</small><br/>
+        
+        <p class="mt-3"><small> Untuk keterangan lebih lanjut hubungi kantor cabang Kami. </small></p>
+        <br/><br/><br/><br/><br/>
+      </div>
+      
+ 
+    </div>
+</div>
 
 
 
--- transfer.php
+++ transfer.php
@@ -15,7 +15,40 @@
 
 <section class="section">
 <!-- Content Start Here -->
+<div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <h4 class="mt-5"><strong>Transfer</strong></h4>
+        <small>Jasa pengiriman uang yang dilaksanakan atas permintaan dan untuk<br/>kepentingan nasabah.</small>
+      
+        <h4 class="mt-5">Keuntungan</h4>
+        <small>1.&nbsp;&nbsp;&nbsp;Memberikan kemudahan dalam transaksi pengiriman uang/</small><br/>
+        <small>2.&nbsp;&nbsp;&nbsp;pembayaran dalam mata uang rupiah dengan biaya yang kompetitif</small><br/>
+        <p class="ml-4"><small>Aman dan cepat.</small><br/></p>
+      </div>
+      
+      <div class="col-sm-5">
+        <img src="asset/pic-transfer.png"  alt="Logo" width="550px" style="left: 686px; height: 360px; margin-bottom:8%;">
+      </div>
 
+      <div class="col-sm-12">
+      <h4>Ketentuan Umum</h4>
+        <small>1.&nbsp;&nbsp;&nbsp;Dilayani di seluruh kantor cabang Bank Mantap.</small><br/>
+        <small>2.&nbsp;&nbsp;&nbsp;Dapat dilaksanakan oleh nasabah atau bukan nasabah.</small><br/>
+        <small>3.&nbsp;&nbsp;&nbsp;Transfer dapat dilaksanakan atas dasar amanat berulang (standing instruction).</small><br/>
+        <small>4.&nbsp;&nbsp;&nbsp;Penerima transfer adalah pemegang rekening Bank Mantap, rekening Bank lain atau diambil tunai. </small><br/>
+        <small>5.&nbsp;&nbsp;&nbsp;Setoran transfer Tunai/Non Tunai.</small><br/>
+      </div>
+
+      <div class="col-sm-12 mt-5">
+      <h4>Jenis Transfer</h4>
+        <small>1.&nbsp;&nbsp;&nbsp;Kliring, adalah layanan transfer antarbank skala nasional dimana jangka waktu penerimaan dana sesuai dengan ketentuan kliring Bank Indonesia.</small><br/>
+        <small>2.&nbsp;&nbsp;&nbsp; RTGS, adalah layanan transfer antarbank skala nasional dimana dana efektif diterima di bank tujuan dalam hitungan menit, selama transaksi dilakukan sebelum batas waktu.</small><br/><br/><br/><br/><br/><br/>
+      </div>
+
+
+    </div>
+</div>
 
 
 
