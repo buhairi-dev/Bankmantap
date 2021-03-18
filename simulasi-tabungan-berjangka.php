db43cac754ad0b7221f5030a20631aeb28a5c12d
Adminlocal <buhairi_mail@yahoo.com>
2021-03-18 16:05:00 +0700

    Update All

--- simulasi-tabungan-berjangka.php
+++ simulasi-tabungan-berjangka.php
@@ -13,34 +13,58 @@
 </ul>
 </section>
 
-<div class="container">
-<h4><strong>Simulasi Tabungan Berjangka</strong></h4>
  
+<style>
+
+.select, .option{
+width: 490px;
+height: 44px;
+background-color:red;
+}
+</style>
+ 
+<div class="container">
+<div class="row">
+  <div class="col-sm-6 text-left">
+  <h4><strong>Simulasi Tabungan Berjangka</strong></h4>
+  </div>
+  
+  <div class="col-sm-5 text-center">
+ 
+  <select name="cars" id="cars" style="background: #FFFFFF 0% 0% no-repeat padding-box;
+        border: 1px solid #D0D8E6;
+        border-radius: 8px;
+        opacity: 1; width: 260px;
+        height: 40px;">
+    <option value="volvo">Pilih simulasi produk lain</option>
+    <option value="Simulasi Tabungan Berjangka">Simulasi Tabungan Berjangka</option>
+    <option value="Simulasi Deposito">Simulasi Deposito</option>
+    <option value="Simulasi kredit Serbaguna Mikro">Simulasi kredit Serbaguna Mikro</option>
+    <option value="Simulasi Kredit Pensiun">Simulasi Kredit Pensiun</option>
+  </select>
+ 
+  </div>
 </div>
 
- 
-<div class="container">
-
-<div class="row mb-5">
+<p><small>Lakukan simulasi perhitungan dana sesuai dengan kebutuhan yang Anda pilih.</small></p>
 
 
-    <div class="col-sm">
-      <div class="card simulasi">
-        <div class="card-body">
-        <br/>
-       
-        <h4>&nbsp;&nbsp;Simulasi Tabungan Berjangka</h4>
+<div class="col-sm-10">
+<div class="card card-body mb-5 mt-5">
 
-        <p class="card-text">Tabungan Samantap Berjangka (TSB) adalah<br/>tabungan setoran wajid bulanan</p>
-        <a class="btn btn-simulasi-flat" role="button" href="#">HITUNG SIMULASI</a> 
-        </div>
-      </div>
+
+
+
+<div class="text-center"><a class="btn btn-simulasi-flat" role="button" href="#">HITUNG SIMULASI</a> </div>
+</div>
+</div>
+     
 	 
  
 
  
-      </div>
-   </div>
+   
+   
    
 </div>
 <br/>
