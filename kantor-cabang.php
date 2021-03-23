9984acd0c225636ac50c571f91ff01456a66bb46
Adminlocal <buhairi_mail@yahoo.com>
2021-03-23 17:14:04 +0700

    Update new

--- kantor-cabang.php
+++ kantor-cabang.php
@@ -21,26 +21,35 @@
 </section>
 
 <div class="container" id="search">
+<script type="text/javascript">
+    function show(obj) {
+      no = obj.options[obj.selectedIndex].value;
+      count = obj.options.length;
+      for(i=1;i<count;i++)
+        document.getElementById('myDiv'+i).style.display = 'none';
+      if(no>0)
+        document.getElementById('myDiv'+no).style.display = 'block';
+    }
+  </script>
     <h4><b>Lokasi Kantor Cabang Mandiri Taspen</b></h4>
         <div class="row mt-4">
-          <div class="col-sm text-left">
-            <form action="home.php">
-            <select class="btn btn-lg" aria-label="Default select example">
-              <option selected>Pilih area kantor cabang (Provinsi)</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+          <div class="col-sm-4 text-left">
+            <form action="">
+            <select class="btn btn-lg" aria-label="Default select example" onchange="show(this)">
+              <option value="0" selected="selected">Pilih area kantor cabang (Provinsi)</option>
+              <option value="1">Sumatera</option>
+              <option value="2">Jawa</option>
+              <option value="3">Bali</option>
+              <option value="4">Kalimantan</option>
             </select>
           </div>
-          <div class="col-sm text-center"><button class="btn btn-lg btn-search" type="submit">Cari</button></div>
+          <!-- <div class="col-sm text-center"><button class="btn btn-lg btn-search" type="submit">Cari</button></div> -->
         </form>
-        </div>
+       
 </div>
 
- 
-<div class="container mt-5">
-
-  <div   class="row mb-5">
+<div id="myDiv1" style="display:none" class="mt-5">
+        <div   class="row mb-5">
 
     <div class="col-sm">
       <div class="card branch">
@@ -97,66 +106,18 @@
  
 
   </div> <!-- End Row -->
+</div>
 
-
-  <div   class="row mb-5">
-
-    <div class="col-sm">
-      <div class="card branch">
-        <div class="card-body">
-        <br/>
-        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
-        <p><small>Wisma Mandiri</small><br/>
-        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
-        <small>021-230265</small></p>
-        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
-        </div>
-      </div>
-		</div>
-
-    <div class="col-sm">
-      <div class="card branch">
-        <div class="card-body">
-        <br/>
-        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
-        <p><small>Wisma Mandiri</small><br/>
-        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
-        <small>021-230265</small></p>
-        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
-        </div>
-      </div>
-		</div>
-
-    <div class="col-sm">
-      <div class="card branch">
-        <div class="card-body">
-        <br/>
-        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
-        <p><small>Wisma Mandiri</small><br/>
-        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
-        <small>021-230265</small></p>
-        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
-        </div>
-      </div>
-		</div>
-
-    <div class="col-sm">
-      <div class="card branch">
-        <div class="card-body">
-        <br/>
-        <img src="asset/logo_mantap.png" style="width: 40%" class="card-img-top mb-2" alt="Image Simulasi"/>
-        <p><small>Wisma Mandiri</small><br/>
-        <small>Jalan M.H. Thamrin No. 5, Kelurahan Kebon Sirih, Kecamatan Menteng</small><br/>
-        <small>021-230265</small></p>
-        <h3><img src="asset/icon/maps.png"  alt="Maps"  style="width: 5%"> &nbsp;Lihat Map</h3>
-        </div>
-      </div>
-		</div>
+<div id="myDiv2" style="display:none">Jawa</div>
+<div id="myDiv3" style="display:none">Bali</div>
+<div id="myDiv4" style="display:none">Kalimantan</div>
+ 
+</div>
 
  
-
-  </div>  <!-- End Row -->
-
+ 
+  
+</div>
 </div>
 <br/>
 <br/>
